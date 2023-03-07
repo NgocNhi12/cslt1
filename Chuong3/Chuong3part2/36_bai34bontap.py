d8b5f2f1edcc7fd42d4c8416a226192e56f017d6
NgcNhi <ngocnhialone2@gmail.com>
2023-03-07 18:25:20 +0700

    add

---
+++ Chuong3/Chuong3part2/36_bai34bontap.py
@@ -0,0 +1,7 @@
+n=int(input("n="))
+i=1
+while i<=n:
+    print(" "*(n-i),end="")
+    print("*"*(2*i-1))
+    i=i+1
+    
--- Chuong3/Chuong3part2/36_bai34ontap.py
+++ Chuong3/Chuong3part2/36_bai34ontap.py
@@ -1,5 +1,8 @@
-
-for i in range(1,n+1):
-    for j in range(i):
-        print(i,end="")
-    print("")
+i=9
+while i>=1:
+    print("$"*i)
+    i=i-1
+#for i in range(1,n+1):
+ #   for j in range(i):
+   #     print(i,end="")
+   # print("")
