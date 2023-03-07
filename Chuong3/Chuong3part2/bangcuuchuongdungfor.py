18f60239ebe4d7c83b90bcb5cafcaa56b2552f43
NgcNhi <ngocnhialone2@gmail.com>
2023-03-07 18:24:56 +0700

    add

--- Chuong3/Chuong3part2/bangcuuchuongdungfor.py
+++ Chuong3/Chuong3part2/bangcuuchuongdungfor.py
@@ -1,19 +1,14 @@
-for i in range(1,10):
-    for j in range(1,10):
-        print(str(i*j).rjust(4), end=" ")
-    print()
-#cach 2
-#print("BANG CUU CHUONG".center(100))
-#for i in range(1,10):
-#   for j in range(1,10):
-#       print(j,'x',i,'=',str(i*j).rjust(2), end=' | ')
-#print()
-#in hoa thi tam giac
-#num = int(input("Number of rows: "))
 
+#num = int(input(" "))
 #for i in range(num):
-   # for j in range(num - i - 1):
-   #     print(end = " ")
-    #for j in range(i+1):
-       # print("*", end=" ")
-   # print()
+   #for j in range(num - i - 1):
+       # print(end = " ")
+        #for j in range(i+1):
+            #print("*", end=" ")
+        #print()
+n=int(input("n="))
+i=1
+for i in range(1,n(2*i-1),i*)
+    print(" "*(n-i),end="")
+    print("*"*(2*i-1))
+    i=i+1
