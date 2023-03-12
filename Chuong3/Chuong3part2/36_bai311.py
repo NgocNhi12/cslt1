a0ab9add9cae5fa9db884d350f02ff0a9e4f5922
NgcNhi <ngocnhialone2@gmail.com>
2023-03-12 21:10:11 +0700

    add

---
+++ Chuong3/Chuong3part2/36_bai311.py
@@ -0,0 +1,8 @@
+a=int(input(""))
+j=0
+k=0
+while a!=0:
+    a=int(input(''))
+for i in range(1,a-1):
+
+    
---
+++ Chuong3/Chuong3part2/36_otapbai34.py
@@ -0,0 +1,17 @@
+a=float(input("a="))
+b=float(input("b="))
+t=str(input("Toan tu:"))
+tt=str(input("Tiep tuc:"))  
+if t=="+":
+    print(a,"+",b,"=",a+b,sep="")
+elif t=="-":
+    print(a,"-",b,"=",a-b,sep='')
+elif t=="*":
+     print(a,"*",b,"=",a*b,sep="")
+elif t=="/":
+    if b==0:
+        print("Khong hop le")
+    elif b!=0:
+        print(a,"/",b,"=",a/b,sep="")
+while tt==
+       
