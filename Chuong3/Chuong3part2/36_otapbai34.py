98d678a340e47da6397526d5a7c7f4c9aefe1d38
NgcNhi <ngocnhialone2@gmail.com>
2023-03-19 07:44:48 +0700

    add

--- Chuong3/Chuong3part2/36_otapbai34.py
+++ Chuong3/Chuong3part2/36_otapbai34.py
@@ -1,17 +1,19 @@
-a=float(input("a="))
-b=float(input("b="))
-t=str(input("Toan tu:"))
-tt=str(input("Tiep tuc:"))  
-if t=="+":
-    print(a,"+",b,"=",a+b,sep="")
-elif t=="-":
-    print(a,"-",b,"=",a-b,sep='')
-elif t=="*":
-     print(a,"*",b,"=",a*b,sep="")
-elif t=="/":
-    if b==0:
-        print("Khong hop le")
-    elif b!=0:
-        print(a,"/",b,"=",a/b,sep="")
-while tt==
-       
+while True:
+    a=float(input("a="))
+    b=float(input("b="))
+    k=str(input("Toan tu:"))
+    if k=="+":
+        print(a,"+",b,"=",a+b,sep="")
+    elif k=="-":
+        print(a,"-",b,"=",a-b,sep='')
+    elif k=="*":
+        print(a,"*",b,"=",a*b,sep="")
+    elif k=="/":
+        if b==0:
+            print("Khong hop le")
+        elif b!=0:
+            print(a,"/",b,"=",a/b,sep="")
+tt=str(input('Tiep tuc:'))
+if tt=='t' or tt=='T':
+    break
+     
